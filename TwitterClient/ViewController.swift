d0b80a94e0fe103899c30771a3c511e56cbb20f2
yamakentoc <yamakentoc@gmail.com>
2018-05-28 20:13:14 +0900

    first commit

---
+++ TwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterClient
+//
+//  Created by 山口賢登 on 2018/05/28.
+//  Copyright © 2018年 山口賢登. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
